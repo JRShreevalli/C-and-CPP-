6b906228ac3219c8d5a7a06a8d310f9ab8f59bc7
J Shreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-05-22 16:21:28 +0530

    Create Pattern_4.cpp

---
+++ Patterns/Pattern_4.cpp
@@ -0,0 +1,32 @@
+Pattern  4 :
+
+RIGHT SIDED TRIANGLE PATTERN 
+[ decreasing space and increasing star ]
+output :  5
+          *
+        * *
+      * * *
+    * * * *
+  * * * * * 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row , col , n ;
+    cin >> n ;
+    for ( row = 1 ; row <= n ; row++ )
+    {
+        for ( col = row ; col <= n ; col++ )
+        {
+            cout  << "  " ;
+        }
+        for ( col = 1 ; col <= row ; col++ )
+        {
+            cout << "*" << " " ;
+        }
+        cout << endl ;
+    }
+    return 0;
+}
